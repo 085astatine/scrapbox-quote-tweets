8cdeacb454689cd6f598f4439b2f1a105ad21d5b
Astatine <astatine085@gmail.com>
2022-07-12 02:01:09 +0900

    Find google-chrome-stable/chromium & set to --chrome-binary

--- web-ext-run.mjs
+++ web-ext-run.mjs
@@ -1,27 +1,61 @@
+import childProcess from 'child_process';
 import path from 'path';
+import util from 'util';
 import webExt from 'web-ext';
 
-const dirname = path.dirname(new URL(import.meta.url).pathname);
-const browser = process.env.TARGET_BROWSER;
-const sourceDir = path.join(dirname, 'build', browser);
-
-const webExtTarget =
-  browser == 'firefox'
-    ? 'firefox-desktop'
-    : browser === 'chrome'
-    ? 'chromium'
-    : undefined;
-
-webExt.cmd
-  .run(
-    {
-      target: webExtTarget,
-      sourceDir,
-    },
-    {
-      shouldExitProgram: false,
+const chromePaths = async () => {
+  const commandNames = ['google-chrome-stable', 'chromium'];
+  const paths = [];
+  const whichResults = commandNames.map((name) => execWhich(name));
+  for await (const result of whichResults) {
+    if (result) {
+      paths.push(result);
     }
-  )
-  .then((extensionRunner) => {
-    console.log(extensionRunner);
-  });
+  }
+  return paths;
+};
+
+const execWhich = (command) => {
+  const exec = util.promisify(childProcess.exec);
+  return exec(`which ${command}`)
+    .then(({ stdout, stderr }) => {
+      return stdout.trim();
+    })
+    .catch((error) => {
+      console.log(`"${command}" is not found`);
+      return null;
+    });
+};
+
+const main = async () => {
+  const dirname = path.dirname(new URL(import.meta.url).pathname);
+  const browser = process.env.TARGET_BROWSER;
+  const sourceDir = path.join(dirname, 'build', browser);
+
+  const webExtTarget =
+    browser == 'firefox'
+      ? 'firefox-desktop'
+      : browser === 'chrome'
+      ? 'chromium'
+      : undefined;
+
+  const webExtOptions = {
+    target: webExtTarget,
+    sourceDir,
+  };
+
+  if (browser === 'chrome') {
+    webExtOptions.chromiumBinary = (await chromePaths())[0];
+  }
+
+  console.log(webExtOptions);
+  webExt.cmd
+    .run(webExtOptions, {
+      shouldExitProgram: false,
+    })
+    .then((extensionRunner) => {
+      console.log(extensionRunner);
+    });
+};
+
+main();
