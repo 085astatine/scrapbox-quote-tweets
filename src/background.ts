e3eecb03070c3fc79cac55d7f908548b42b5eaf5
Astatine <astatine085@gmail.com>
2022-07-29 02:27:26 +0900

    Add listener to browser.tabs.onUpdated

--- src/background.ts
+++ src/background.ts
@@ -1,5 +1,16 @@
+import browser from 'webextension-polyfill';
 import { loggerProvider } from './logger';
 
 const logger = loggerProvider.getCategory('background');
 
 logger.info('background script');
+
+const tabListener = (
+  tabID: number,
+  changeInfo: browser.Tabs.OnUpdatedChangeInfoType,
+  tab: browser.Tabs.Tab
+) => {
+  logger.info(`tab ID: ${tabID}, URL: ${tab.url}`, changeInfo);
+};
+
+browser.tabs.onUpdated.addListener(tabListener);
