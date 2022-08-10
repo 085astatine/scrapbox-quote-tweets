9830eabcc5835408c7da6abaf74c2379ab7d1b18
Astatine <astatine085@gmail.com>
2022-08-10 19:06:33 +0900

    show node type as string

--- src/content-twitter.ts
+++ src/content-twitter.ts
@@ -18,13 +18,48 @@ const getNode = (xpath: string): Node | null => {
   );
   const node = result.singleNodeValue;
   if (node !== null) {
-    logger.info('node is found', { type: node.nodeType, name: node.nodeName });
+    logger.info(
+      `node: name=${node.nodeName}, type=${showNodeType(node.nodeType)})`
+    );
   } else {
     logger.error('node is not found');
   }
   return node;
 };
 
+const nodeTypeToString = (nodeType: number): string => {
+  switch (nodeType) {
+    case Node.ELEMENT_NODE: // 1
+      return 'Element';
+    case Node.ATTRIBUTE_NODE: // 2
+      return 'Attribute';
+    case Node.TEXT_NODE: // 3
+      return 'Text';
+    case Node.CDATA_SECTION_NODE: // 4
+      return 'CDataSection';
+    case Node.PROCESSING_INSTRUCTION_NODE: // 7
+      return 'ProcessingInstruction';
+    case Node.COMMENT_NODE: // 8
+      return 'Comment';
+    case Node.DOCUMENT_NODE: // 9
+      return 'Document';
+    case Node.DOCUMENT_TYPE_NODE: // 10
+      return 'DocumentType';
+    case Node.DOCUMENT_FRAGMENT_NODE: // 11
+      return 'DocumentFragment';
+    // deprecated
+    case Node.ENTITY_REFERENCE_NODE: // 5
+    case Node.ENTITY_NODE: // 6
+    case Node.NOTATION_NODE: // 12
+      return 'deprecated';
+    default:
+      return 'undefined';
+  }
+};
+
+const showNodeType = (nodeType: number): string => {
+  return `${nodeType}(${nodeTypeToString(nodeType)})`;
+};
 // observer
 const observerCallback = (records: MutationRecord[]): void => {
   logger.info('mutation observer callback');
