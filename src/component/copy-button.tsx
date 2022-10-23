2850258ef160be56e9fe8c456df4e68a0c68f019
Astatine <astatine085@gmail.com>
2022-10-24 02:34:13 +0900

    Fade in/out tooltips

--- src/component/copy-button.tsx
+++ src/component/copy-button.tsx
@@ -1,8 +1,11 @@
 import { arrow, offset, shift, useFloating } from '@floating-ui/react-dom';
+import classNames from 'classnames';
 import React from 'react';
 import browser from 'webextension-polyfill';
 import { TweetCopyResponseMessage } from '../lib/message';
 import ScrapboxIcon from './logo/scrapbox.svg';
+
+type TooltipVisibility = 'none' | 'fade-in' | 'visible' | 'fade-out';
 
 export interface CopyButtonProps {
   tweetID: bigint | null;
@@ -11,6 +14,8 @@ export interface CopyButtonProps {
 export const CopyButton: React.FC<CopyButtonProps> = (props) => {
   // state
   const [isCopied, setIsCopied] = React.useState(false);
+  const [tooltipVisibility, setTooltipVisibility] =
+    React.useState<TooltipVisibility>('none');
   // floting
   const arrowRef = React.useRef(null);
   const {
@@ -24,10 +29,39 @@ export const CopyButton: React.FC<CopyButtonProps> = (props) => {
     placement: 'top',
     middleware: [offset(10), shift(), arrow({ element: arrowRef })],
   });
+  // effect: tooltip visibility
+  React.useEffect(() => {
+    switch (tooltipVisibility) {
+      case 'none':
+        break;
+      case 'fade-in': {
+        const timeoutID = setTimeout(
+          () => setTooltipVisibility('visible'),
+          200
+        );
+        return () => clearTimeout(timeoutID);
+      }
+      case 'visible': {
+        const timeoutID = setTimeout(
+          () => setTooltipVisibility('fade-out'),
+          2000
+        );
+        return () => clearTimeout(timeoutID);
+      }
+      case 'fade-out': {
+        const timeoutID = setTimeout(() => setTooltipVisibility('none'), 200);
+        return () => clearTimeout(timeoutID);
+      }
+      default: {
+        const _: never = tooltipVisibility;
+        return _;
+      }
+    }
+  }, [tooltipVisibility]);
   // click
   const onClick = (event: React.MouseEvent<HTMLDivElement>) => {
     event.stopPropagation();
-    alert(`tweet ID: ${props.tweetID}`);
+    // alert(`tweet ID: ${props.tweetID}`);
     // send message to background
     browser.runtime
       .sendMessage({
@@ -41,6 +75,7 @@ export const CopyButton: React.FC<CopyButtonProps> = (props) => {
           if (!message.ok) {
             console.log(message.message);
           }
+          setTooltipVisibility('fade-in');
         }
       });
   };
@@ -60,21 +95,28 @@ export const CopyButton: React.FC<CopyButtonProps> = (props) => {
           height={undefined}
         />
       </div>
-      <div
-        className="tooltip"
-        ref={floating}
-        style={{ position: strategy, top: y ?? 0, left: x ?? 0 }}>
-        Tooltip
+      {tooltipVisibility !== 'none' ? (
         <div
-          className="arrow"
-          ref={arrowRef}
-          style={{
-            position: strategy,
-            top: arrowY !== null ? `${arrowY}px` : '',
-            left: arrowX !== null ? `${arrowX}px` : '',
-          }}
-        />
-      </div>
+          className={classNames('tooltip', {
+            'fade-in': tooltipVisibility === 'fade-in',
+            'fade-out': tooltipVisibility === 'fade-out',
+          })}
+          ref={floating}
+          style={{ position: strategy, top: y ?? 0, left: x ?? 0 }}>
+          Tooltip
+          <div
+            className="arrow"
+            ref={arrowRef}
+            style={{
+              position: strategy,
+              top: arrowY !== null ? `${arrowY}px` : '',
+              left: arrowX !== null ? `${arrowX}px` : '',
+            }}
+          />
+        </div>
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
