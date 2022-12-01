a2aea592cb81dc18d0a3c7cf5407fcbece4dadc2
Astatine <astatine085@gmail.com>
2022-12-01 21:10:30 +0900

    Use redux-logger

--- src/content-twitter/store.ts
+++ src/content-twitter/store.ts
@@ -1,6 +1,21 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { stateSlice } from './state';
+import { Middleware } from 'redux';
+import { createLogger } from 'redux-logger';
+import { State, stateSlice } from './state';
+
+const middlewares: Middleware<State>[] = [];
+
+// Logger
+if (process.env.NODE_ENV === 'development') {
+  const logger = createLogger({
+    collapsed: true,
+    diff: true,
+  });
+  middlewares.push(logger);
+}
 
 export const store = configureStore({
   reducer: stateSlice.reducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(...middlewares),
 });
