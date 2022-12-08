b4bc8caf2e931c3070b13814e75b16ed6d9539cb
Astatine <astatine085@gmail.com>
2022-12-08 16:05:53 +0900

    Add saveTweets()

--- src/lib/storage.ts
+++ src/lib/storage.ts
@@ -1,5 +1,13 @@
 import browser from 'webextension-polyfill';
 import { logger } from './logger';
+import { Tweet } from './tweet';
+import { toTweetIDKey } from './tweet-id-key';
+
+export const saveTweets = async (tweets: Tweet[]): Promise<void> => {
+  await browser.storage.local.set(
+    Object.fromEntries(tweets.map((tweet) => [toTweetIDKey(tweet.id), tweet]))
+  );
+};
 
 export const dumpStorage = async (): Promise<void> => {
   const data = await browser.storage.local.get();
