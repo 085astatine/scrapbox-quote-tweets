bf5efa395124f39466fba27a56e61df2a73e765a
Astatine <astatine085@gmail.com>
2022-12-08 17:57:20 +0900

    Validate with JSON Schema before setting to storage

--- src/lib/storage.ts
+++ src/lib/storage.ts
@@ -1,9 +1,21 @@
 import browser from 'webextension-polyfill';
+import { tweetsJSONSchema } from '../jsonschema/tweet';
+import { JSONSchemaValidationError } from '../validate-json/jsonschema-validation-error';
+import validateTweets from '../validate-json/validate-tweets';
 import { logger } from './logger';
 import { Tweet } from './tweet';
 import { toTweetIDKey } from './tweet-id-key';
 
 export const saveTweets = async (tweets: Tweet[]): Promise<void> => {
+  // JSON Schema validation
+  if (!validateTweets(tweets)) {
+    throw new JSONSchemaValidationError(
+      tweetsJSONSchema,
+      tweets,
+      validateTweets.errors
+    );
+  }
+  // set to storage
   await browser.storage.local.set(
     Object.fromEntries(tweets.map((tweet) => [toTweetIDKey(tweet.id), tweet]))
   );
