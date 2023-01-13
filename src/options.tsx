c0d24437db90756a753bc75238c169adf9ae1ec5
Astatine <astatine085@gmail.com>
2023-01-13 17:41:48 +0900

    Add options.scss

--- src/options.tsx
+++ src/options.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Root } from './options/component/root';
+import './style/options.scss';
 
 const root = document.getElementById('root');
 if (root !== null) {
