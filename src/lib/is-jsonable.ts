e4a9382f83bfa6a53ad46d49c5edca16beaa5288
Astatine <astatine085@gmail.com>
2023-01-28 19:39:53 +0900

    Check null, boolean, string, number

--- src/lib/is-jsonable.ts
+++ src/lib/is-jsonable.ts
@@ -9,7 +9,21 @@ export type JSONableValue =
   | { [key: string]: JSONableValue };
 
 export const isJSONable = (value: any): value is JSONableValue => {
-  return false;
+  switch (typeof value) {
+    case 'boolean':
+      return true;
+    case 'string':
+      return true;
+    case 'number':
+      return !Number.isNaN(value);
+    case 'object':
+      if (value === null) {
+        return true;
+      }
+      return false;
+    default:
+      return false;
+  }
 };
 
 /* eslint-enable @typescript-eslint/no-explicit-any */
