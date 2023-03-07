9fbcf50694fae4345b639761d3795dcd60e09622
Astatine <astatine085@gmail.com>
2023-03-07 13:47:45 +0900

    Define TweetField

--- src/lib/tweet-to-string.ts
+++ src/lib/tweet-to-string.ts
@@ -5,3 +5,18 @@ export const tweetToString = (tweet: Tweet, template: string): string => {
   logger.debug('tweet to string', tweet, template);
   return '';
 };
+
+type TweetField =
+  | 'tweet.id'
+  | 'tweet.timestamp'
+  | 'user.id'
+  | 'user.name'
+  | 'user.username';
+
+const tweetFields: readonly TweetField[] = [
+  'tweet.id',
+  'tweet.timestamp',
+  'user.id',
+  'user.name',
+  'user.username',
+];
