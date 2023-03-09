013052f1a0cbcca6d1374ce2422bdd4328ebc187
Astatine <astatine085@gmail.com>
2023-03-09 14:13:43 +0900

    Fill out TweetTemplate

--- src/lib/tweet-to-string.ts
+++ src/lib/tweet-to-string.ts
@@ -1,11 +1,49 @@
-import { logger } from './logger';
 import { Tweet } from './tweet';
-import { TweetTemplate, parseTweetTemplate } from './tweet-template';
+import {
+  TemplateElement,
+  TweetField,
+  TweetTemplate,
+  parseTweetTemplate,
+} from './tweet-template';
 
 export const tweetToString = (
   tweet: Tweet,
   template: TweetTemplate
 ): string => {
-  logger.debug('tweet to string', tweet, parseTweetTemplate(template));
-  return '';
+  const parsedTemplate = parseTweetTemplate(template);
+  const filledOutTemplate = parsedTemplate.tweet
+    .map((element) => fillTweetTemplateElement(element, tweet))
+    .join('');
+  return filledOutTemplate;
+};
+
+const fillTweetTemplateElement = (
+  templateElement: TemplateElement<TweetField>,
+  tweet: Tweet
+): string => {
+  switch (templateElement.type) {
+    case 'text':
+      return templateElement.text;
+    case 'placeholder':
+      switch (templateElement.field) {
+        case 'tweet.id':
+          return tweet.id;
+        case 'tweet.timestamp':
+          return `${tweet.timestamp}`;
+        case 'user.id':
+          return tweet.author.id;
+        case 'user.name':
+          return tweet.author.name;
+        case 'user.username':
+          return tweet.author.username;
+        default: {
+          const _: never = templateElement.field;
+          return _;
+        }
+      }
+    default: {
+      const _: never = templateElement;
+      return _;
+    }
+  }
 };
