d52041ff4f730b7b9e425dd0e11ac398a0414ac9
Astatine <astatine085@gmail.com>
2023-09-29 17:50:07 +0900

    Move .scss

--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import '../style/popup.scss';
 import { Root } from './component/root';
+import './index.scss';
 
 const root = document.getElementById('root');
 if (root !== null) {
