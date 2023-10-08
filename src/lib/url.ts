456862ed8429f9b2d4e8528dba87d6ac5bc0eb39
Astatine <astatine085@gmail.com>
2023-10-09 02:03:18 +0900

    Add expandTCoURL()

--- src/lib/url.ts
+++ src/lib/url.ts
@@ -1,3 +1,16 @@
+import { Logger, logger as defaultLogger } from '~/lib/logger';
+
+export const isTCoURL = (url: string): boolean => {
+  try {
+    return new URL(url).hostname === 't.co';
+  } catch (error) {
+    if (!(error instanceof TypeError)) {
+      throw error;
+    }
+  }
+  return false;
+};
+
 export const formatTCoURL = (url: string): string => {
   try {
     const address = new URL(url);
@@ -10,6 +23,30 @@ export const formatTCoURL = (url: string): string => {
     }
   }
   return url;
+};
+
+export const expandTCoURL = async (
+  url: string,
+  logger: Logger = defaultLogger,
+): Promise<string | null> => {
+  logger.debug(`expand ${url}`);
+  if (!isTCoURL(url)) {
+    logger.warn(`URL("${url}") is not 'https:/t.co/...'`);
+    return null;
+  }
+  const expandedURL = await fetch(url)
+    .then((response) => response.text())
+    .then((text) => {
+      const parser = new DOMParser();
+      const dom = parser.parseFromString(text, 'text/html');
+      return dom.title;
+    })
+    .catch((error) => {
+      logger.warn(`Failed to expand "${url}"`, error);
+      return null;
+    });
+  logger.debug(`Expanded URL is "${expandedURL}"`);
+  return expandedURL;
 };
 
 export const formatTwimgURL = (url: string): string => {
