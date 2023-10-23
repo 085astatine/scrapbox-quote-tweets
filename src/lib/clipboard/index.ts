b9db40a29f9cefd9c166adc1ae38c7aa5ba1a110
Astatine <astatine085@gmail.com>
2023-10-23 17:32:19 +0900

    Add interface TrashboxRecord

--- src/lib/clipboard/index.ts
+++ src/lib/clipboard/index.ts
@@ -1,1 +1,8 @@
+import { TweetID } from '../tweet';
+
 export { ClipboardWindows, setupClipboardWindows } from './windows';
+
+export interface TrashboxRecord {
+  timestamp: number;
+  tweetIDs: TweetID[];
+}
