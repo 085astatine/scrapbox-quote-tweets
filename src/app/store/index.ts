cc04aa852bab9167989aea3b29f76cc93920ba3e
Astatine <astatine085@gmail.com>
2024-01-07 23:39:44 +0900

    Devide slice into tweet and settings

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -1,251 +1,14 @@
-import { PayloadAction, configureStore, createSlice } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import { Middleware } from 'redux';
 import { createLogger } from 'redux-logger';
-import { isValidTimezone } from '~/lib/datetime';
-import {
-  Hostname,
-  Settings,
-  defaultSettings,
-  isHostname,
-} from '~/lib/settings';
+import { defaultSettings } from '~/lib/settings';
 import { loadSettings } from '~/lib/storage/settings';
 import { loadTrashbox, loadTweetsNotInTrashbox } from '~/lib/storage/trashbox';
-import { DeletedTweets } from '~/lib/tweet/deleted-tweets';
-import { DeletedTweetsSort, TweetSort } from '~/lib/tweet/sort-tweets';
-import { Tweet } from '~/lib/tweet/tweet';
-import { toArray } from '~/lib/utility';
-
-// state
-type EditingSettings = Partial<
-  Omit<Settings, 'tweetSort' | 'deletedTweetsSort'>
->;
-
-type SettingsErrors = Partial<Record<keyof EditingSettings, string[]>>;
-
-export interface State {
-  tweets: Tweet[];
-  trashbox: DeletedTweets[];
-  selectedTweets: Tweet[];
-  selectedDeletedTweets: Tweet[];
-  settings: Settings;
-  settingsEditing: EditingSettings;
-  settingsErrors: SettingsErrors;
-}
-
-const initialState = (): State => {
-  return {
-    tweets: [],
-    trashbox: [],
-    selectedTweets: [],
-    selectedDeletedTweets: [],
-    settings: defaultSettings(),
-    settingsEditing: {},
-    settingsErrors: {},
-  };
-};
-
-export interface Initializer {
-  tweets: Tweet[];
-  trashbox: DeletedTweets[];
-  settings: Settings;
-}
-
-const slice = createSlice({
-  name: 'clipboard',
-  initialState: initialState(),
-  reducers: {
-    initialize(state: State, action: PayloadAction<Initializer>): void {
-      state.tweets = [...action.payload.tweets];
-      state.trashbox = [...action.payload.trashbox];
-      state.settings = { ...action.payload.settings };
-    },
-    selectTweet(state: State, action: PayloadAction<Tweet>): void {
-      const exist = state.selectedTweets.find(
-        (tweet) => tweet.id === action.payload.id,
-      );
-      if (!exist) {
-        state.selectedTweets.push(action.payload);
-      }
-    },
-    unselectTweet(state: State, action: PayloadAction<Tweet>): void {
-      const index = state.selectedTweets.findIndex(
-        (tweet) => tweet.id === action.payload.id,
-      );
-      if (index !== -1) {
-        state.selectedTweets.splice(index, 1);
-      }
-    },
-    selectAllTweets(state: State): void {
-      state.selectedTweets = [...state.tweets];
-    },
-    unselectAllTweets(state: State): void {
-      state.selectedTweets = [];
-    },
-    moveToTrashbox(state: State, action: PayloadAction<number>): void {
-      // remove from tweets
-      state.selectedTweets.forEach((target) => {
-        const index = state.tweets.findIndex((tweet) => tweet.id === target.id);
-        if (index !== -1) {
-          state.tweets.splice(index, 1);
-        }
-      });
-      // add to trashbox
-      state.trashbox.push({
-        deleted_at: action.payload,
-        tweets: [...state.selectedTweets],
-      });
-      // clear selected tweets
-      state.selectedTweets = [];
-    },
-    selectDeletedTweet(
-      state: State,
-      action: PayloadAction<Tweet | Tweet[]>,
-    ): void {
-      toArray(action.payload).forEach((selectedTweet) => {
-        const exist = state.selectedDeletedTweets.find(
-          (tweet) => tweet.id === selectedTweet.id,
-        );
-        if (!exist) {
-          state.selectedDeletedTweets.push(selectedTweet);
-        }
-      });
-    },
-    unselectDeletedTweet(
-      state: State,
-      action: PayloadAction<Tweet | Tweet[]>,
-    ): void {
-      toArray(action.payload).forEach((selectedTweet) => {
-        const index = state.selectedDeletedTweets.findIndex(
-          (tweet) => tweet.id === selectedTweet.id,
-        );
-        if (index !== -1) {
-          state.selectedDeletedTweets.splice(index, 1);
-        }
-      });
-    },
-    selectAllDeletedTweets(state: State) {
-      state.selectedDeletedTweets = state.trashbox
-        .map((deletedTweets) => deletedTweets.tweets)
-        .flat();
-    },
-    unselectAllDeletedTweets(state: State) {
-      state.selectedDeletedTweets = [];
-    },
-    restoreSelectedDeletedTweets(state: State): void {
-      // restore to tweets
-      state.tweets.push(...state.selectedDeletedTweets);
-      // remove from trashbox
-      state.trashbox = state.trashbox.reduce<DeletedTweets[]>(
-        (result, element) => {
-          element.tweets = element.tweets.filter((tweet) =>
-            state.selectedDeletedTweets.every(
-              (selected) => selected.id !== tweet.id,
-            ),
-          );
-          if (element.tweets.length > 0) {
-            result.push(element);
-          }
-          return result;
-        },
-        [],
-      );
-      // clear selected deleted tweets
-      state.selectedDeletedTweets = [];
-    },
-    deleteSelectedDeletedTweets(state: State): void {
-      // remove from trashbox
-      state.trashbox = state.trashbox.reduce<DeletedTweets[]>(
-        (result, element) => {
-          element.tweets = element.tweets.filter((tweet) =>
-            state.selectedDeletedTweets.every(
-              (selected) => selected.id !== tweet.id,
-            ),
-          );
-          if (element.tweets.length > 0) {
-            result.push(element);
-          }
-          return result;
-        },
-        [],
-      );
-      // clear selected deleted tweets
-      state.selectedDeletedTweets = [];
-    },
-    updateTweetSort(state: State, action: PayloadAction<TweetSort>): void {
-      state.settings.tweetSort = action.payload;
-    },
-    updateDeletedTweetsSort(
-      state: State,
-      action: PayloadAction<DeletedTweetsSort>,
-    ): void {
-      state.settings.deletedTweetsSort = action.payload;
-    },
-    updateSettings(state: State): void {
-      // reset errors
-      state.settingsErrors = {};
-      // hostname (base URL)
-      if ('hostname' in state.settingsEditing) {
-        const newHostname = state.settingsEditing.hostname;
-        if (!isHostname(newHostname)) {
-          state.settingsErrors.hostname = [
-            `"${newHostname}" is not valid hostname.`,
-          ];
-        }
-      }
-      // timezone
-      if ('timezone' in state.settingsEditing) {
-        const newTimezone = state.settingsEditing.timezone;
-        if (!isValidTimezone(newTimezone)) {
-          state.settingsErrors.timezone = [
-            `"${newTimezone}" is not valid timezone.`,
-            'Please input the time zone in the IANA database.',
-            'Examples: "UTC", "Asia/Tokyo", "America/New_York"',
-          ];
-        }
-      }
-      // datetimeFormat: no validation
-      // update if theare is no error
-      if (Object.keys(state.settingsErrors).length === 0) {
-        state.settings = {
-          ...state.settings,
-          ...state.settingsEditing,
-        };
-        state.settingsEditing = {};
-      }
-    },
-    resetEditingSettings(state: State): void {
-      state.settingsEditing = {};
-      state.settingsErrors = {};
-    },
-    updateHostname(state: State, action: PayloadAction<Hostname>): void {
-      editSettings(state, 'hostname', action.payload);
-    },
-    updateTimezone(state: State, action: PayloadAction<string>): void {
-      editSettings(state, 'timezone', action.payload);
-    },
-    updateDatetimeFormat(state: State, action: PayloadAction<string>): void {
-      editSettings(state, 'datetimeFormat', action.payload);
-    },
-  },
-});
-
-const editSettings = <Key extends keyof EditingSettings>(
-  state: State,
-  key: Key,
-  value: EditingSettings[Key],
-): void => {
-  if (state.settings[key] !== value) {
-    state.settingsEditing[key] = value;
-  } else if (key in state.settingsEditing) {
-    delete state.settingsEditing[key];
-  }
-};
-
-// actions
-export const actions: Readonly<typeof slice.actions> = slice.actions;
+import { settings, settingsActions } from './settings';
+import { tweet, tweetActions } from './tweet';
 
 // store
-const middlewares: Middleware<State>[] = [];
+const middlewares: Middleware[] = [];
 
 if (process.env.NODE_ENV === 'development') {
   middlewares.push(
@@ -258,17 +21,28 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 export const store = configureStore({
-  reducer: slice.reducer,
+  reducer: {
+    tweet: tweet.reducer,
+    settings: settings.reducer,
+  },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(...middlewares),
 });
 
 export type Store = typeof store;
 
+export type State = ReturnType<typeof store.getState>;
+
+export const actions = {
+  tweet: tweetActions,
+  settings: settingsActions,
+} as const;
+
 // Initialize store with data loaded from storage
 export const initializeStoreWithStorage = async (): Promise<void> => {
   const tweets = await loadTweetsNotInTrashbox();
   const trashbox = await loadTrashbox();
   const settings = (await loadSettings()) ?? defaultSettings();
-  store.dispatch(actions.initialize({ tweets, trashbox, settings }));
+  store.dispatch(actions.tweet.initialize({ tweets, trashbox }));
+  store.dispatch(actions.settings.initialize(settings));
 };
