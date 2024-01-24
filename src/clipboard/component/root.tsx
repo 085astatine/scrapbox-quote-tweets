0e518f985f582a3404d97e95283415b67eb7da58
Astatine <astatine085@gmail.com>
2024-01-25 00:19:31 +0900

    Delete close icon from clipboard.html

--- src/clipboard/component/root.tsx
+++ src/clipboard/component/root.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import browser from 'webextension-polyfill';
 import { App } from '~/app';
 import { Store } from '~/app/store';
-import CloseIcon from '~/icon/bootstrap/x.svg';
 
 export interface RootProps {
   store: Store;
@@ -18,15 +16,9 @@ export const Root: React.FC<RootProps> = ({ store }: RootProps) => {
 };
 
 const Header: React.FC = () => {
-  const close = () => {
-    browser.runtime.sendMessage({ type: 'Clipboard/CloseRequest' });
-  };
   return (
     <div className="header">
       <div className="title">Scrapbox Copy Tweets</div>
-      <button className="close btn btn-danger" onClick={close}>
-        <CloseIcon className="icon" width={undefined} height={undefined} />
-      </button>
     </div>
   );
 };
