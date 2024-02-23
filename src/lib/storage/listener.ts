cda0d955eb580041f42737487e673fcf1f2ea08f
Astatine <astatine085@gmail.com>
2024-02-23 14:17:51 +0900

    Notify storage listeners of trashbox changes

--- src/lib/storage/listener.ts
+++ src/lib/storage/listener.ts
@@ -1,6 +1,6 @@
 import browser from 'webextension-polyfill';
 import { Logger } from '../logger';
-import { Tweet, TweetID } from '../tweet/types';
+import { DeletedTweetID, Tweet, TweetID } from '../tweet/types';
 import { isTweetIDKey, toTweetID } from './tweet-id-key';
 
 type OnChangedListener = (
@@ -19,8 +19,21 @@ export interface TweetChanges {
   updated?: UpdatedTweet[];
 }
 
+export interface UpdatedDeletedTweetID {
+  id: TweetID;
+  before: DeletedTweetID;
+  after: DeletedTweetID;
+}
+
+export interface TrashboxChanges {
+  added?: DeletedTweetID[];
+  deleted?: DeletedTweetID[];
+  updated?: UpdatedDeletedTweetID[];
+}
+
 export interface StorageListenerArguments {
   tweet?: TweetChanges;
+  trashbox?: TrashboxChanges;
 }
 
 export const createStorageListener = (
@@ -29,7 +42,7 @@ export const createStorageListener = (
 ): OnChangedListener => {
   return (changes: browser.Storage.StorageAreaOnChangedChangesType) => {
     logger?.debug('Storage changes', changes);
-    // diff
+    // tweet changes
     const addedTweets: Tweet[] = [];
     const deletedTweets: Tweet[] = [];
     const updatedTweets: UpdatedTweet[] = [];
@@ -50,14 +63,20 @@ export const createStorageListener = (
         }
       }
     }
-    // listener arguments
-    const tweetChanges: TweetChanges = {
+    const tweet: TweetChanges = {
       ...(addedTweets.length > 0 && { added: addedTweets }),
       ...(deletedTweets.length > 0 && { deleted: deletedTweets }),
       ...(updatedTweets.length > 0 && { updated: updatedTweets }),
     };
+    // trashbox changes
+    const trashbox = trashboxChanges(
+      changes['trashbox']?.oldValue,
+      changes['trashbox']?.newValue,
+    );
+    // listener arguments
     const listenerArgs: StorageListenerArguments = {
-      ...(Object.keys(tweetChanges).length > 0 && { tweet: tweetChanges }),
+      ...(Object.keys(tweet).length > 0 && { tweet }),
+      ...(Object.keys(trashbox).length > 0 && { trashbox }),
     };
     logger?.debug('listener arguments', listenerArgs);
     // execute listener
@@ -67,4 +86,57 @@ export const createStorageListener = (
 
 export const addStorageListener = (listener: OnChangedListener): void => {
   browser.storage.local.onChanged.addListener(listener);
+};
+
+const trashboxChanges = (
+  oldValue: DeletedTweetID[] | undefined,
+  newValue: DeletedTweetID[] | undefined,
+): TrashboxChanges => {
+  if (oldValue === undefined || oldValue.length === 0) {
+    if (newValue === undefined || newValue.length === 0) {
+      return {};
+    } else {
+      return { added: newValue };
+    }
+  } else if (newValue === undefined || newValue.length === 0) {
+    return { deleted: oldValue };
+  }
+  const changes: Map<
+    TweetID,
+    { before?: DeletedTweetID; after?: DeletedTweetID }
+  > = new Map(
+    oldValue.map((deletedTweet) => [
+      deletedTweet.tweet_id,
+      { before: deletedTweet },
+    ]),
+  );
+  newValue.forEach((deletedTweet) => {
+    const change = changes.get(deletedTweet.tweet_id);
+    if (change !== undefined) {
+      change.after = deletedTweet;
+    } else {
+      changes.set(deletedTweet.tweet_id, { after: deletedTweet });
+    }
+  });
+  const added: DeletedTweetID[] = [];
+  const deleted: DeletedTweetID[] = [];
+  const updated: UpdatedDeletedTweetID[] = [];
+  for (const [tweetID, { before, after }] of changes.entries()) {
+    if (before === undefined) {
+      if (after !== undefined) {
+        added.push(after);
+      }
+    } else {
+      if (after === undefined) {
+        deleted.push(before);
+      } else if (before.deleted_at !== after.deleted_at) {
+        updated.push({ id: tweetID, before, after });
+      }
+    }
+  }
+  return {
+    ...(added.length > 0 && { added }),
+    ...(deleted.length > 0 && { deleted }),
+    ...(updated.length > 0 && { updated }),
+  };
 };
